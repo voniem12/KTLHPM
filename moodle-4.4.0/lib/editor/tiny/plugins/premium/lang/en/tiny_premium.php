b79037bba5ed2151990887a45fc2677f0f09c649
truong <truong@truong.truong>
2025-11-26 23:31:33 -0500

    Initial upload

---
+++ moodle-4.4.0/lib/editor/tiny/plugins/premium/lang/en/tiny_premium.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tiny_premium', language 'en'.
+ *
+ * @package     tiny_premium
+ * @category    string
+ * @copyright   2023 David Woloszyn <david.woloszyn@moodle.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'TinyMCE Premium';
+$string['apikey'] = 'API key';
+$string['apikey_desc'] = '<p>Your API key is available on your <a href="https://www.tiny.cloud">Tiny Cloud</a> account page if you have purchased a subscription, or if you are on a free trial.</p><p>See the list of available TinyMCE Premium features for Moodle in the documentation <a href="https://docs.moodle.org/en/TinyMCE_editor">TinyMCE editor</a>.</p>';
+$string['premium:accesspremium'] = 'Access TinyMCE Premium features';
+$string['privacy:metadata'] = 'The TinyMCE Premium plugin does not store any personal data.';
